e2812d523b74334bea882c1fc61328d52810b487
GrunCrow <albamrqz751@gmail.com>
2022-05-12 15:57:50 +0200

    Color Recognition Code

--- color_recognition.py
+++ color_recognition.py
@@ -1,10 +1,6 @@
-import numpy as np
 import pandas as pd
 import cv2
 
-# define image
-# todo create loop to load more images (?)
-img = cv2.imread("color_image.jpg")
 
 # teach the colours -> take csv file from github and load, add the columns name
 # (https://github.com/codebrainz/color-names/blob/master/output/colors.csv)
@@ -14,6 +10,11 @@ csv = pd.read_csv('colors.csv', names=index, header=None)
 # global variables
 clicked = False
 r = g = b = x_pos = y_pos = 0
+played = True
+color_rec = 'Black'
+
+# default image
+img = cv2.imread("color_image.jpg")
 
 
 def recognize_color(red, green, blue):
@@ -27,8 +28,12 @@ def recognize_color(red, green, blue):
     return c_name
 
 
+def load_img(image):
+    img = image
+
+
 # mouse click function
-def mouse_click(event, x, y, flags, param):
+def double_click(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDBLCLK:
         global b, g, r, x_pos, y_pos, clicked
         clicked = True
@@ -39,34 +44,22 @@ def mouse_click(event, x, y, flags, param):
         g = int(g)
         r = int(r)
 
-
-cv2.namedWindow('Color Recognition App')
-cv2.setMouseCallback('Color Recognition App', mouse_click)
-
-while 1:
-    cv2.imshow("Color Recognition App", img)
-
-    if clicked:
-        # cv2.rectangle(image, startpoint, endpoint, color, thickness)-1 fills entire rectangle
-        cv2.rectangle(img, (20, 20), (750, 60), (b, g, r), -1)
-        # Creating text string to display( Color name and RGB values )
-        text = recognize_color(r, g, b) + ' R=' + str(r) + ' G=' + str(g) + ' B=' + str(b)
-
-        # cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
-        cv2.putText(img, text, (50, 50), 2, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
-        # For very light colours we will display text in black colour
-        if r + g + b >= 600:
-            cv2.putText(img, text, (50, 50), 2, 0.8, (0, 0, 0), 2, cv2.LINE_AA)
-
-        clicked = False
-
-    # Break the loop when user hits 'esc' key
-    if cv2.waitKey(20) & 0xFF == 27:
-        break
-
-cv2.destroyAllWindows()
+def single_click(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        global b, g, r, x_pos, y_pos, clicked
+        clicked = True
+        x_pos = x
+        y_pos = y
+        b, g, r = img[y, x]
+        b = int(b)
+        g = int(g)
+        r = int(r)
 
 
-# todo Add Audio
-# todo Video Version
-# todo Final Version
+# function for detecting left mouse click
+def click(event, x, y, flags, param):
+    global clicked, b, g, r, x_pos, y_pos
+    if event == cv2.EVENT_LBUTTONDOWN:
+        x_pos = x
+        y_pos = y
+        clicked = True
